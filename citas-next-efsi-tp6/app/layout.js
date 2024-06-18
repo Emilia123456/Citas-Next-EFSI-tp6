6c31af43da507ac7a545028a6c277c21314c46f6
EMILIA INÉS ARMENO <47697893@ort>
2024-06-18 08:46:41 -0300

    prueb 3a

--- citas-next-efsi-tp6/app/layout.js
+++ citas-next-efsi-tp6/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
+
 import "./skeleton.css";
 import "./globals.css";
 import { Navbar } from "/components/Navbar/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body >
         <Navbar />
         {children}
       </body>
